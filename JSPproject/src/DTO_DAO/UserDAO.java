ac1d49e6acdc95d548327158433941283436d3da
RewJuBin <rewjb@naver.com>
2018-12-27 10:20:12 +0900

    DTO DAO 생성 및 외부 라이브러리 이동

---
+++ JSPproject/src/DTO_DAO/UserDAO.java
@@ -0,0 +1,11 @@
+package DTO_DAO;
+
+public class UserDAO {
+	
+	
+	
+	
+	
+	
+
+}
---
+++ JSPproject/src/DTO_DAO/UserDTO.java
@@ -0,0 +1,22 @@
+package DTO_DAO;
+
+public class UserDTO {
+	
+	String id;
+	//회원 아이디
+	String pw;
+	//회원 비밀번호
+	String name;
+	//회원 이름
+	String tel;
+	//회원 전화번호
+	String addr;
+	//회원 주소
+	String email;
+	//회원 이메일
+	
+	
+	
+	
+
+}
